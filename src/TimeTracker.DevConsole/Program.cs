3c67e39535719efca03019cd817fce584867b196
Richard Niemand <niemand.richard@gmail.com>
2020-12-28 22:11:10 -0700

    Adding start of logging

---
+++ src/TimeTracker.DevConsole/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using Rn.NetCore.Common.Logging;
+
+namespace TimeTracker.DevConsole
+{
+  class Program
+  {
+    private static IServiceProvider _serviceProvider;
+    private static ILoggerAdapter<Program> _logger;
+
+    static void Main(string[] args)
+    {
+      ConfigureDI();
+
+      _logger.Info("Hello world");
+
+      Console.WriteLine("Hello World!");
+    }
+
+
+    // DI related methods
+    private static void ConfigureDI()
+    {
+      var services = new ServiceCollection();
+
+      var config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .Build();
+
+      services
+        .AddSingleton<IConfiguration>(config)
+        .AddSingleton(typeof(ILoggerAdapter<>), typeof(LoggerAdapter<>))
+        .AddLogging(loggingBuilder =>
+        {
+          // configure Logging with NLog
+          loggingBuilder.ClearProviders();
+          loggingBuilder.SetMinimumLevel(LogLevel.Trace);
+          loggingBuilder.AddNLog(config);
+        });
+
+      _serviceProvider = services.BuildServiceProvider();
+      _logger = _serviceProvider.GetService<ILoggerAdapter<Program>>();
+    }
+  }
+}

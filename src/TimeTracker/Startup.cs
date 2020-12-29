7b433f74f5f537e0b4dc8580b7e28cbacfb7051d
Richard Niemand <richard.niemand@derivco.co.za>
2020-12-29 14:39:06 -0700

    Adding start of Angular Material

--- src/TimeTracker/Startup.cs
+++ src/TimeTracker/Startup.cs
@@ -16,18 +16,16 @@ namespace TimeTracker
 
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllersWithViews();
-      // In production, the Angular files will be served from this directory
+
       services.AddSpaStaticFiles(configuration =>
       {
         configuration.RootPath = "ClientApp/dist";
       });
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
@@ -56,14 +54,12 @@ namespace TimeTracker
 
       app.UseSpa(spa =>
       {
-              // To learn more about options for serving an Angular SPA from ASP.NET Core,
-              // see https://go.microsoft.com/fwlink/?linkid=864501
-
-              spa.Options.SourcePath = "ClientApp";
+        spa.Options.SourcePath = "ClientApp";
 
         if (env.IsDevelopment())
         {
-          spa.UseAngularCliServer(npmScript: "start");
+          //spa.UseAngularCliServer(npmScript: "start");
+          spa.UseProxyToSpaDevelopmentServer("http://localhost:4200/");
         }
       });
     }

c93e660ea2fc47addf06d402ffe239fb47b60a97
Richard Niemand <richard.niemand@derivco.co.za>
2021-01-04 09:36:07 -0700

    Ticking timers

--- src/TimeTracker/ClientApp/src/app/components/ui/time-logger/list-timers/list-timers.component.ts
+++ src/TimeTracker/ClientApp/src/app/components/ui/time-logger/list-timers/list-timers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { RawTrackedTimeDto, TrackedTimeClient } from 'src/app/time-tracker-api';
 
@@ -7,16 +7,21 @@ import { RawTrackedTimeDto, TrackedTimeClient } from 'src/app/time-tracker-api';
   templateUrl: './list-timers.component.html',
   styleUrls: ['./list-timers.component.css']
 })
-export class ListTimersComponent implements OnInit {
+export class ListTimersComponent implements OnInit, OnDestroy {
   timers: RawTrackedTimeDto[] = [];
+  flipFlop: boolean = false;
+
+  private _interval: any = null;
 
   constructor(
     private trackedTimeClient: TrackedTimeClient,
     private uiService: UiService
   ) { }
-
+  
   ngOnInit(): void {
     this.timers = [];
+
+    this._interval = setInterval(() => { this.flipFlop = !this.flipFlop; }, 1000);
 
     this.trackedTimeClient.getRunningTimers().toPromise().then(
       (timers: RawTrackedTimeDto[]) => {
@@ -24,6 +29,12 @@ export class ListTimersComponent implements OnInit {
       },
       this.uiService.handleClientError
     );
+  }
+
+  ngOnDestroy(): void {
+    if(this._interval) {
+      clearInterval(this._interval);
+    }
   }
 
 }

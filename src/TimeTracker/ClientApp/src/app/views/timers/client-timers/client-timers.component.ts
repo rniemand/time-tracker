033daa74b44cd909b12b16e96c4450150e8dc642
Richard Niemand <richard.niemand@derivco.co.za>
2021-01-13 13:59:58 -0700

    Working on UI

--- src/TimeTracker/ClientApp/src/app/views/timers/client-timers/client-timers.component.ts
+++ src/TimeTracker/ClientApp/src/app/views/timers/client-timers/client-timers.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router';
+import { UiService } from 'src/app/services/ui.service';
+import { ClientDto, ClientsClient, ProductDto, ProductsClient } from 'src/app/time-tracker-api';
 
 @Component({
   selector: 'app-client-timers',
@@ -6,10 +12,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-timers.component.css']
 })
 export class ClientTimersComponent implements OnInit {
+  displayedColumns: string[] = ['client', 'name', 'controls'];
+  dataSource = new MatTableDataSource<ProductDto>();
+  clientId: number = 0;
+  client?: ClientDto = undefined;
+  clientName: string = 'Client';
 
-  constructor() { }
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(
+    private route: ActivatedRoute,
+    private uiService: UiService,
+    private clietsClient: ClientsClient,
+    private productsClient: ProductsClient
+  ) { }
 
   ngOnInit(): void {
+    this.clientId = this.route.snapshot.params?.clientId ?? 0;
+
+    this.uiService.showLoader();
+    this.loadClientInfo()
+      .then(this.loadProjects, this.uiService.handleClientError)
+      .then(
+        () => { this.uiService.hideLoader(); },
+        this.uiService.handleClientError
+      );
   }
 
+  // Internal methods
+  private loadClientInfo = () => {
+    return new Promise<void>((resolve, reject) => {
+      this.clietsClient.getClientById(this.clientId).toPromise().then(
+        (client: ClientDto) => {
+          this.client = client;
+          this.clientName = client?.clientName ?? 'Client';
+          resolve();
+        },
+        (error: any) => { reject(error); }
+      );
+    });
+  }
+
+  private loadProjects = () => {
+    return new Promise<void>((resolve, reject) => {
+      this.productsClient.getAllProducts(this.clientId).toPromise().then(
+        (products: ProductDto[]) => {
+          this.dataSource = new MatTableDataSource(products);
+          this.dataSource.sort = this.sort;
+          this.dataSource.paginator = this.paginator;
+
+          resolve();
+        },
+        (error: any) => { reject(error); }
+      );
+    });
+  }
 }

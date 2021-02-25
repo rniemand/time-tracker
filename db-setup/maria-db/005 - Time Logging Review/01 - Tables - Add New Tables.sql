1793de34b5aba3a55ac13aff8ec0b1d7c811ab98
Richard Niemand <richard.niemand@derivco.co.za>
2021-02-25 12:16:47 -0700

    Adding base DB

--- db-setup/maria-db/005 - Time Logging Review/01 - Tables - Add New Tables.sql
+++ db-setup/maria-db/005 - Time Logging Review/01 - Tables - Add New Tables.sql
@@ -12,8 +12,8 @@ CREATE TABLE `TimeSheet_Date` (
 	INDEX `UserId` (`UserId`) USING BTREE,
 	INDEX `ClientId` (`ClientId`) USING BTREE,
 	INDEX `DayOfWeek` (`DayOfWeek`) USING BTREE,
-	CONSTRAINT `FK_TimeSheet_Date_Clients` FOREIGN KEY (`ClientId`) REFERENCES `TimeTrackerDev`.`Clients` (`ClientId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
-	CONSTRAINT `FK_TimeSheet_Date_Users` FOREIGN KEY (`UserId`) REFERENCES `TimeTrackerDev`.`Users` (`UserId`) ON UPDATE RESTRICT ON DELETE RESTRICT
+	CONSTRAINT `FK_TimeSheet_Date_Clients` FOREIGN KEY (`ClientId`) REFERENCES `TimeTracker`.`Clients` (`ClientId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
+	CONSTRAINT `FK_TimeSheet_Date_Users` FOREIGN KEY (`UserId`) REFERENCES `TimeTracker`.`Users` (`UserId`) ON UPDATE RESTRICT ON DELETE RESTRICT
 )
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
@@ -38,11 +38,11 @@ CREATE TABLE `TimeSheet_Rows` (
 	INDEX `ProductId` (`ProductId`) USING BTREE,
 	INDEX `ProjectId` (`ProjectId`) USING BTREE,
 	INDEX `ClientId` (`ClientId`) USING BTREE,
-	CONSTRAINT `FK_TimeSheet_Rows_Clients` FOREIGN KEY (`ClientId`) REFERENCES `TimeTrackerDev`.`Clients` (`ClientId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
-	CONSTRAINT `FK_TimeSheet_Rows_Products` FOREIGN KEY (`ProductId`) REFERENCES `TimeTrackerDev`.`Products` (`ProductId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
-	CONSTRAINT `FK_TimeSheet_Rows_Projects` FOREIGN KEY (`ProjectId`) REFERENCES `TimeTrackerDev`.`Projects` (`ProjectId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
-	CONSTRAINT `FK_TimeSheet_Rows_TimeSheet_Date` FOREIGN KEY (`DateId`) REFERENCES `TimeTrackerDev`.`TimeSheet_Date` (`DateId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
-	CONSTRAINT `FK_TimeSheet_Rows_Users` FOREIGN KEY (`UserId`) REFERENCES `TimeTrackerDev`.`Users` (`UserId`) ON UPDATE RESTRICT ON DELETE RESTRICT
+	CONSTRAINT `FK_TimeSheet_Rows_Clients` FOREIGN KEY (`ClientId`) REFERENCES `TimeTracker`.`Clients` (`ClientId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
+	CONSTRAINT `FK_TimeSheet_Rows_Products` FOREIGN KEY (`ProductId`) REFERENCES `TimeTracker`.`Products` (`ProductId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
+	CONSTRAINT `FK_TimeSheet_Rows_Projects` FOREIGN KEY (`ProjectId`) REFERENCES `TimeTracker`.`Projects` (`ProjectId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
+	CONSTRAINT `FK_TimeSheet_Rows_TimeSheet_Date` FOREIGN KEY (`DateId`) REFERENCES `TimeTracker`.`TimeSheet_Date` (`DateId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
+	CONSTRAINT `FK_TimeSheet_Rows_Users` FOREIGN KEY (`UserId`) REFERENCES `TimeTracker`.`Users` (`UserId`) ON UPDATE RESTRICT ON DELETE RESTRICT
 )
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
@@ -70,12 +70,12 @@ CREATE TABLE `TimeSheet_Entries` (
 	INDEX `ClientId` (`ClientId`) USING BTREE,
 	INDEX `ProductId` (`ProductId`) USING BTREE,
 	INDEX `ProjectId` (`ProjectId`) USING BTREE,
-	CONSTRAINT `FK_TimeSheet_Entries_Clients` FOREIGN KEY (`ClientId`) REFERENCES `TimeTrackerDev`.`Clients` (`ClientId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
-	CONSTRAINT `FK_TimeSheet_Entries_Products` FOREIGN KEY (`ProductId`) REFERENCES `TimeTrackerDev`.`Products` (`ProductId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
-	CONSTRAINT `FK_TimeSheet_Entries_Projects` FOREIGN KEY (`ProjectId`) REFERENCES `TimeTrackerDev`.`Projects` (`ProjectId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
-	CONSTRAINT `FK_TimeSheet_Entries_TimeSheet_Date` FOREIGN KEY (`DateId`) REFERENCES `TimeTrackerDev`.`TimeSheet_Date` (`DateId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
-	CONSTRAINT `FK_TimeSheet_Entries_TimeSheet_Rows` FOREIGN KEY (`RowId`) REFERENCES `TimeTrackerDev`.`TimeSheet_Rows` (`RowId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
-	CONSTRAINT `FK_TimeSheet_Entries_Users` FOREIGN KEY (`UserId`) REFERENCES `TimeTrackerDev`.`Users` (`UserId`) ON UPDATE RESTRICT ON DELETE RESTRICT
+	CONSTRAINT `FK_TimeSheet_Entries_Clients` FOREIGN KEY (`ClientId`) REFERENCES `TimeTracker`.`Clients` (`ClientId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
+	CONSTRAINT `FK_TimeSheet_Entries_Products` FOREIGN KEY (`ProductId`) REFERENCES `TimeTracker`.`Products` (`ProductId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
+	CONSTRAINT `FK_TimeSheet_Entries_Projects` FOREIGN KEY (`ProjectId`) REFERENCES `TimeTracker`.`Projects` (`ProjectId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
+	CONSTRAINT `FK_TimeSheet_Entries_TimeSheet_Date` FOREIGN KEY (`DateId`) REFERENCES `TimeTracker`.`TimeSheet_Date` (`DateId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
+	CONSTRAINT `FK_TimeSheet_Entries_TimeSheet_Rows` FOREIGN KEY (`RowId`) REFERENCES `TimeTracker`.`TimeSheet_Rows` (`RowId`) ON UPDATE RESTRICT ON DELETE RESTRICT,
+	CONSTRAINT `FK_TimeSheet_Entries_Users` FOREIGN KEY (`UserId`) REFERENCES `TimeTracker`.`Users` (`UserId`) ON UPDATE RESTRICT ON DELETE RESTRICT
 )
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
--- db-setup/maria-db/005 - Time Logging Review/03 - Alter - Tables.sql
+++ db-setup/maria-db/005 - Time Logging Review/03 - Alter - Tables.sql
@@ -157,8 +157,8 @@ ALTER TABLE `TimeSheet_Date`
 	DROP FOREIGN KEY `FK_TimeSheet_Date_Clients`,
 	DROP FOREIGN KEY `FK_TimeSheet_Date_Users`;
 ALTER TABLE `TimeSheet_Date`
-	ADD CONSTRAINT `FK_TimeSheet_Date_Clients` FOREIGN KEY (`ClientId`) REFERENCES `TimeTrackerDev`.`Clients` (`ClientId`) ON UPDATE RESTRICT ON DELETE CASCADE,
-	ADD CONSTRAINT `FK_TimeSheet_Date_Users` FOREIGN KEY (`UserId`) REFERENCES `TimeTrackerDev`.`Users` (`UserId`) ON UPDATE RESTRICT ON DELETE CASCADE;
+	ADD CONSTRAINT `FK_TimeSheet_Date_Clients` FOREIGN KEY (`ClientId`) REFERENCES `TimeTracker`.`Clients` (`ClientId`) ON UPDATE RESTRICT ON DELETE CASCADE,
+	ADD CONSTRAINT `FK_TimeSheet_Date_Users` FOREIGN KEY (`UserId`) REFERENCES `TimeTracker`.`Users` (`UserId`) ON UPDATE RESTRICT ON DELETE CASCADE;
 	
 ALTER TABLE `TimeSheet_Rows`
 	DROP COLUMN `EntryDate`,
